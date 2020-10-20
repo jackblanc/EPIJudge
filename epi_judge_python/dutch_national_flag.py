e83b319c3a941da50a1791e920ffbd24b1b4d539
Jack Blanc <jblanc222@gmail.com>
2020-10-20 08:50:38 -0400

    dutch national flag

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -7,16 +7,59 @@ from test_framework.test_utils import enable_executor_hook
 
 RED, WHITE, BLUE = range(3)
 
+# Naive implementation with O(N^2) runtime
+# def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
+#     pivot = A[pivot_index]
+#
+#     # first pass - group elements smaller than pivot
+#     for i in range(len(A)):
+#         for j in range(i + 1, len(A)):
+#             if A[j] < pivot:
+#                 A[i], A[j] = A[j], A[i]
+#
+#     for i in reversed(range(len(A))):
+#         for j in reversed(range(i)):
+#             if A[j] > pivot:
+#                 A[i], A[j] = A[j], A[i]
+#
 
+
+# O(n) runtime complexity
+# def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
+#     pivot = A[pivot_index]
+#
+#     smaller = 0
+#     for i in range(len(A)):
+#         if A[i] < pivot:
+#             A[i], A[smaller] = A[smaller], A[i]
+#             smaller += 1
+#
+#     larger = len(A) - 1
+#     for i in reversed(range(len(A))):
+#         if A[i] > pivot:
+#             A[i], A[larger] = A[larger], A[i]
+#             larger -= 1
+
+# one pass O(n)
 def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
     pivot = A[pivot_index]
 
-    # first pass - group elements smaller than pivot
-    for i in range(len(A)):
-        for j in range(i + 1, len(A)):
-            if A[j] < pivot:
-                A[i], A[j] = A[j], A[i]
-    return
+    smaller = 0
+    equal = 0
+    larger = len(A)
+
+    while equal < larger:
+        if A[equal] < pivot:
+            A[equal], A[smaller] = A[smaller], A[equal]
+            smaller += 1
+            equal += 1
+        elif A[equal] == pivot:
+            equal += 1
+        else:
+            larger -= 1
+            A[equal], A[larger] = A[larger], A[equal]
+
+
 
 
 @enable_executor_hook
